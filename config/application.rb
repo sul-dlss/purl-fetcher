bac5f375ec206bc9c29f195f4b27c2d94283e0d8
Chris Beer <chris@cbeer.info>
2022-01-03 10:47:32 -0800

    Update to Rails 7

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative 'boot'
 
 require 'rails/all'
-require 'logger'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,20 +9,14 @@ Bundler.require(*Rails.groups)
 module PurlFetcher
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Add in files in lib/
-    config.eager_load_paths << Rails.root.join('lib')
-
-    config.version = File.read('VERSION') # read from VERSION file at base of website
-    config.app_name = 'PurlFetcher'
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
-
-UpdatingLogger = Logger.new(Settings.FILENAME_UPDATING_LOG)
-UpdatingLogger.level = Logger::INFO

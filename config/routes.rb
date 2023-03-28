1842d52a607911b716b2f8268419691627a6b808
Justin Coyne <jcoyne@justincoyne.com>
2023-03-28 10:59:49 -0500

    Remove invalid route syntax

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'v1/docs#stats'
 
   scope module: :v1, constraints: ApiConstraint.new(version: 1), defaults: { format: :json } do
-    resource :docs, :only => [:get] do
+    resource :docs, only: [] do
       get 'deletes'
       get 'changes'
     end

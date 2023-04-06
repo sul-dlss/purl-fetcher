2b122f540295c836ca525dc71cf6b8d5ee94f000
Justin Coyne <jcoyne@justincoyne.com>
2023-04-06 13:56:55 -0500

    Remove unused routes
    Fixes #565

--- app/views/v1/purls/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.purls do
-  json.array! @purls, partial: 'v1/purls/purl', as: :purl
-end
-json.partial! 'shared/paginate', locals: { object: @purls }
--- spec/features/purl_targets_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe 'Purl targets', integration: true, type: :request do
-  it 'true targets are not available on deleted documents' do
-    get purl_path 'druid:ee111ff2222'
-    parsed_response = JSON.parse(response.body)
-    expect(parsed_response).not_to include 'true_targets'
-  end
-end

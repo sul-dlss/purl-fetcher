8740d98e07b5d73e13ed433232e11bf545861c0d
Justin Coyne <jcoyne@justincoyne.com>
2024-05-06 16:21:07 -0500

    Remove unused code

--- app/consumers/purl_updates_consumer.rb
+++ app/consumers/purl_updates_consumer.rb
@@ -2,22 +2,10 @@ class PurlUpdatesConsumer < Racecar::Consumer
   subscribes_to "purl-updates"
 
   # Update the Purl database record with Cocina data passed in the message
-  def process(message) # rubocop:disable Metrics/MethodLength
+  def process(message)
     json = JSON.parse(message.value)
-    cocina_object = nil
-    actions = nil
-    if json.key?('cocina')
-      cocina_object = Cocina::Models.build(json['cocina'])
-      actions = json['actions']
-    else
-      Honeybadger.notify("In dead code path. We should remove this once we stop seeing this code called.")
-      cocina_object = Cocina::Models.build(json)
-      actions = { 'index' => [], 'delete' => [] }.tap do |releases|
-        cocina_object.administrative.releaseTags.each do |tag|
-          releases[tag.release ? 'index' : 'delete'] << tag.to
-        end
-      end
-    end
+    cocina_object = Cocina::Models.build(json['cocina'])
+    actions = json['actions']
     purl = Purl.find_by!(druid: cocina_object.externalIdentifier)
     PurlCocinaUpdater.new(purl, cocina_object, actions).update
 

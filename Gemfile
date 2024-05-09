78d317c66caddc1329fd560a244df80f774719c4
Justin Coyne <jcoyne@justincoyne.com>
2024-05-09 15:59:20 -0500

    Test the generated XML against the existing XML

--- Gemfile
+++ Gemfile
@@ -25,10 +25,10 @@ gem 'honeybadger'
 gem 'okcomputer' # for monitoring
 
 gem 'config'
+gem 'equivalent-xml'
 
 group :test do
   gem 'capybara'
-  gem 'equivalent-xml'
   gem 'rspec-rails', '~> 6.0'
   gem 'simplecov', require: false
 end
--- app/consumers/purl_updates_consumer.rb
+++ app/consumers/purl_updates_consumer.rb
@@ -11,10 +11,25 @@ class PurlUpdatesConsumer < Racecar::Consumer
     actions = json['actions']
     purl = Purl.find_by!(druid: cocina_object.externalIdentifier)
     PurlCocinaUpdater.new(purl, cocina_object, actions).update
+    purl.produce_indexer_log_message
+
+    test_public_xml_generation(cocina_object)
   rescue Cocina::Models::ValidationError => e
     Honeybadger.notify(e, context: { json: })
+
   rescue StandardError => e
     Honeybadger.notify(e, context: { json: })
     raise e
+  end
+
+  # If we don't see any errors from this in a few weeks, we can write this to the filesystem'
+  def test_public_xml_generation(public_cocina)
+    thumbnail_service = ThumbnailService.new(public_cocina)
+    generated_xml = Publish::PublicXmlService.new(public_cocina:, thumbnail_service:).to_xml
+    path = "#{DruidTools::PurlDruid.new(public_cocina.externalIdentifier, Settings.filesystems.purl_root).path}/public"
+    existing_xml = File.read(path)
+    Honeybadger.notify("Generated XML is not equivalent", context: { druid: public_cocina.externalIdentifier }) unless EquivalentXml.equivalent?(generated_xml, existing_xml)
+  rescue StandardError => e
+    Honeybadger.notify(e, context: { druid: public_cocina.externalIdentifier })
   end
 end

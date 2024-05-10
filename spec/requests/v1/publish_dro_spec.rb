1124e5d3d2e50b3422cfea9c5b5e0266bdcb9996
Aaron Collier <aaron.collier@stanford.edu>
2024-05-10 16:34:05 -0700

    Add prototype publish API
    Validate cocina version
    
    Update test for publish api
    
    WIP
    
    WIP - remove unnecessary code while reworking file shelving
    
    WIP - remove unused code for file shelving
    
    Remove files that are on disk but not in cocina
    
    Cleanup linting

---
+++ spec/requests/v1/publish_dro_spec.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'base64'
+
+RSpec.describe 'Publish a DRO' do
+  let(:bare_druid) { 'bc123df4567' }
+  let(:druid) { "druid:#{bare_druid}" }
+  let(:dro) { build(:dro_with_metadata, id: druid).new(structural:) }
+  let(:request) { dro.to_json }
+  let(:contains) do
+    [
+      Cocina::Models::FileSet.new(
+        externalIdentifier: 'bc123df4567_2',
+        type: Cocina::Models::FileSetType.file,
+        label: 'text file',
+        version: 1,
+        structural: Cocina::Models::FileSetStructural.new(
+          contains: [
+            Cocina::Models::File.new(
+              externalIdentifier: signed_id,
+              type: Cocina::Models::ObjectType.file,
+              label: 'the text file',
+              filename: 'file2.txt',
+              version: 1,
+              hasMessageDigests: [
+                { type: 'md5', digest: Base64.decode64(blob.checksum).unpack1('H*') }
+              ]
+            )
+          ]
+        )
+      )
+    ]
+  end
+  let(:structural) do
+    Cocina::Models::DROStructural.new(
+      contains:
+    )
+  end
+  let(:blob) { create(:singleton_blob_with_file) }
+  let(:signed_id) do
+    ActiveStorage.verifier.generate(blob.id, purpose: :blob_id)
+  end
+
+  before do
+    allow(Racecar).to receive(:produce_sync)
+  end
+
+  context 'when a cocina object is received' do
+    it 'creates the cocina json file for the resource' do
+      post '/v1/resources',
+           params: request,
+           headers: { 'Content-Type' => 'application/json', 'Authorization' => "Bearer #{jwt}" }
+      expect(response).to be_created
+      expect(File).to exist('tmp/purl_doc_cache/bc/123/df/4567/cocina.json')
+      expect(File).to exist('tmp/stacks/bc/123/df/4567/file2.txt')
+    end
+  end
+
+  context 'when blob not found for file' do
+    let(:signed_id) { ActiveStorage.verifier.generate('thisisinvalid', purpose: :blob_id) }
+
+    it 'returns 500' do
+      post '/v1/resources',
+           params: request,
+           headers: { 'Content-Type' => 'application/json', 'Authorization' => "Bearer #{jwt}" }
+      expect(response).to have_http_status(:server_error)
+      expect(JSON.parse(response.body)['errors'][0]['title']).to eq 'Error matching uploading files to file parameters.' # rubocop:disable Rails/ResponseParsedBody
+    end
+  end
+
+  context 'when file is not found in the cocina object' do
+    before do
+      File.write('tmp/stacks/bc/123/df/4567/file3.txt', 'hello world')
+    end
+
+    it 'deletes the file' do
+      expect(File).to exist('tmp/stacks/bc/123/df/4567/file3.txt')
+      post '/v1/resources',
+           params: request,
+           headers: { 'Content-Type' => 'application/json', 'Authorization' => "Bearer #{jwt}" }
+      expect(response).to be_created
+      expect(File).not_to exist('tmp/stacks/bc/123/df/4567/file3.txt')
+    end
+  end
+end

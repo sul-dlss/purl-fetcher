7ec22a1a7b0f11bcdf6664ce8fc4f4bcef4f893e
Justin Coyne <jcoyne@justincoyne.com>
2024-06-25 13:27:20 -0500

    Persist the rspec state
    So we can use --only-failures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -101,4 +101,9 @@ RSpec.configure do |config|
    # test failures related to randomization by passing the same `--seed` value
    # as the one that triggered the failure.
   #  Kernel.srand config.seed
+
+  # Allows RSpec to persist some state between runs in order to support
+  # the `--only-failures` and `--next-failure` CLI options. We recommend
+  # you configure your source control system to ignore this file.
+  config.example_status_persistence_file_path = "spec/examples.txt"
 end

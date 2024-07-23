bd167d26de022d252cf47ffae48365f4aee4f748
Chris Beer <cabeer@stanford.edu>
2024-07-23 15:53:42 -0700

    Use the param name druid consistently for the ReleasedController.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'v1/docs#stats'
 
   scope module: :v1, constraints: ApiConstraint.new(version: 1), defaults: { format: :json } do
-    resources :released, only: :show
+    resources :released, only: :show, param: :release_tag
 
     resources :purls, only: [:destroy, :show], param: :druid do
       member do

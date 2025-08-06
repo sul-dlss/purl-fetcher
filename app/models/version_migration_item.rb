47062027d71980b95dc845505f4a239bfc9f09f1
Justin Coyne <jcoyne@justincoyne.com>
2025-08-06 08:47:31 -0500

    Analyze every purl to see if we need to migrate it or not
    fixes #1011

--- app/models/version_migration_item.rb
+++ app/models/version_migration_item.rb
@@ -1,9 +1,23 @@
 class VersionMigrationItem < ApplicationRecord
-  validates :status, inclusion: { in: ['not_analyzed', 'queued_for_analysis', 'found_legacy', 'found_version', 'migrated'] }
+  validates :status, inclusion: { in: ['not_analyzed', 'error', 'found_legacy', 'found_version', 'migrated'] }
 
   def self.create_all
     Purl.find_each do |purl|
       VersionMigrationItem.create(druid: purl.druid)
+    end
+  end
+
+  def self.analyze_purls
+    VersionMigrationItem.where(status: 'not_analyzed').find_each do |version_migration_item|
+      stacks_path = DruidTools::PurlDruid.new(version_migration_item.druid, Settings.filesystems.stacks_root).pathname
+      raise FileNotFound, stacks_path.to_s unless stacks_path.exist?
+
+      obj = VersionedFilesService::Object.new(version_migration_item.druid)
+      status = Pathname.new(obj.versions_manifest_path).exist? ? 'found_version' : 'found_legacy'
+      version_migration_item.update(status:)
+    rescue StandardError => e
+      Honeybadger.notify(e, context: { druid: version_migration_item.druid }, tags: 'analysis')
+      version_migration_item.update(status: 'error')
     end
   end
 end

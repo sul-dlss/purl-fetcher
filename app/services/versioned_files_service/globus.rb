a0bfa1ecf14abadaff389950fc47464ecf64fad0
Marlo Longley <mlongley@stanford.edu>
2025-08-06 17:44:38 -0400

    Create VersionedFilesService::Globus to allow linking
    Link from versioned md5 content to stacks/globus
    Closes #1453

---
+++ app/services/versioned_files_service/globus.rb
@@ -0,0 +1,41 @@
+class VersionedFilesService
+  # Hardlink files in the /globus directory for the specified druids
+  class Globus
+    def initialize
+      @globus_root = Settings.filesystems.globus_root
+      @druid_list = Settings.globus.druid_list
+    end
+
+    def link_all_druids
+      @druid_list.each do |druid|
+        link_druid(druid)
+      end
+    end
+
+    def link_druid(druid)
+      object = VersionedFilesService::Object.new(druid)
+      link_globus_files(object)
+    end
+
+    private
+
+    def link_globus_files(object)
+      # Only link current version files
+      version = object.version_manifest.head_version
+      return unless version
+
+      globus_path = globus_path_for(object.druid)
+      file_details = object.file_details_by_md5_for_version(object.druid, version)
+
+      file_details.each do |file|
+        source_file_path = object.content_path_for(md5: file.md5)
+        globus_file_path = globus_path / file.filename
+        LinkSupport.link(source_file_path, globus_file_path)
+      end
+    end
+
+    def globus_path_for(druid)
+      DruidTools::PurlDruid.new(druid, @globus_root).pathname
+    end
+  end
+end
---
+++ spec/services/versioned_files_service/globus_spec.rb
@@ -0,0 +1,113 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe VersionedFilesService::Globus do
+  let(:service) { described_class.new }
+  let(:druid) { 'bf070wx6289' }
+  let(:prefixed_druid) { "druid:#{druid}" }
+
+  let!(:purl_object) { create(:purl, druid: prefixed_druid) }
+
+  let(:purl_pathname) { 'tmp/purl_root' }
+  let(:stacks_pathname) { 'tmp/stacks' }
+  let(:globus_pathname) { 'tmp/stacks/globus' }
+
+  let(:object) { VersionedFilesService::Object.new(druid) }
+  let(:versions_path) { object.versions_path }
+  let(:globus_object_path) { "#{globus_pathname}/bf/070/wx/6289" }
+
+  let(:cocina_object) do
+    build(:dro_with_metadata, id: prefixed_druid).new(access: { view: 'world', download: 'world' }, structural:
+        Cocina::Models::DROStructural.new(
+          contains: [
+            Cocina::Models::FileSet.new(
+              externalIdentifier: 'bc123df4567_2',
+              type: Cocina::Models::FileSetType.file,
+              label: 'text file',
+              version: 1,
+              structural: Cocina::Models::FileSetStructural.new(
+                contains: [
+                  Cocina::Models::File.new(
+                    externalIdentifier: '1234',
+                    type: Cocina::Models::ObjectType.file,
+                    label: 'the regular file',
+                    filename: 'file2.txt',
+                    size: 9, # write_version uses the file name for the file content
+                    version: 1,
+                    hasMessageDigests: [
+                      { type: 'md5', digest: '3e25960a79dbc69b674cd4ec67a72c62' }
+                    ],
+                    administrative: {
+                      publish: true,
+                      shelve: true
+                    }
+                  ),
+                  Cocina::Models::File.new(
+                    externalIdentifier: '1234',
+                    type: Cocina::Models::ObjectType.file,
+                    label: 'the hierarchical file',
+                    filename: 'files/file2.txt',
+                    size: 15, # write_version uses the file name for the file content
+                    version: 1,
+                    hasMessageDigests: [
+                      { type: 'md5', digest: '5997de4d5abb55f21f652aa61b8f3aaf' }
+                    ],
+                    administrative: {
+                      publish: true,
+                      shelve: true
+                    }
+                  )
+                ]
+              )
+            )
+          ]
+        ))
+  end
+
+  before do
+    FileUtils.rm_rf(stacks_pathname)
+    FileUtils.rm_rf(purl_pathname)
+    FileUtils.rm_rf(globus_pathname)
+    allow(Settings.filesystems).to receive_messages(stacks_root: stacks_pathname, purl_root: purl_pathname, globus_root: globus_pathname)
+  end
+
+  after do
+    FileUtils.rm_rf(stacks_pathname)
+    FileUtils.rm_rf(purl_pathname)
+    FileUtils.rm_rf(globus_pathname)
+  end
+
+  describe '#link_all_druids' do
+    before do
+      # Just test one druid, but make sure we can read the list.
+      allow(Settings.globus).to receive(:druid_list).and_return([druid])
+      FileUtils.mkdir_p(versions_path.to_s)
+
+      manifest = { head: 2 }
+      File.write("#{versions_path}/versions.json", manifest.to_json)
+
+      cocina_json = create(:public_json, purl: purl_object).data
+      File.write("#{versions_path}/cocina.json", cocina_json)
+
+      write_version(content_path: object.content_path, versions_path:, stacks_object_path: object.stacks_object_path, cocina_object: cocina_object)
+    end
+
+    it 'creates hardlinks in the globus directory' do
+      described_class.new.link_druid(druid)
+
+      expect(File).to exist("#{globus_object_path}/file2.txt")
+      expect(File).to exist("#{globus_object_path}/files/file2.txt")
+
+      expect(File.identical?(
+               object.content_path_for(md5: '3e25960a79dbc69b674cd4ec67a72c62'),
+               Pathname.new(globus_object_path) / 'file2.txt'
+             )).to be true
+
+      expect(File.identical?(
+               object.content_path_for(md5: '5997de4d5abb55f21f652aa61b8f3aaf'),
+               Pathname.new(globus_object_path) / 'files/file2.txt'
+             )).to be true
+    end
+  end
+end

02b28f45169c599caf1f99fb0a27d7012dea1022
Justin Coyne <jcoyne@justincoyne.com>
2025-08-06 10:53:34 -0500

    Use a defined exception
    FileNotFound is not a defined class

--- app/models/version_migration_item.rb
+++ app/models/version_migration_item.rb
@@ -1,5 +1,5 @@
 class VersionMigrationItem < ApplicationRecord
-  validates :status, inclusion: { in: ['not_analyzed', 'error', 'found_legacy', 'found_version', 'migrated'] }
+  validates :status, inclusion: { in: ['not_analyzed', 'error', 'found_legacy', 'found_legacy_collection', 'found_version', 'migrated'] }
 
   def self.create_all
     Purl.where(deleted_at: nil).find_each do |purl|
@@ -10,11 +10,17 @@ class VersionMigrationItem < ApplicationRecord
   def self.analyze_purls
     VersionMigrationItem.where(status: 'not_analyzed').find_each do |version_migration_item|
       stacks_path = DruidTools::PurlDruid.new(version_migration_item.druid, Settings.filesystems.stacks_root).pathname
-      raise FileNotFound, stacks_path.to_s unless stacks_path.exist?
 
-      obj = VersionedFilesService::Object.new(version_migration_item.druid)
-      status = Pathname.new(obj.versions_manifest_path).exist? ? 'found_version' : 'found_legacy'
-      version_migration_item.update(status:)
+      if stacks_path.exist?
+        obj = VersionedFilesService::Object.new(version_migration_item.druid)
+        status = Pathname.new(obj.versions_manifest_path).exist? ? 'found_version' : 'found_legacy'
+        version_migration_item.update(status:)
+      else
+        purl = Purl.find_by(druid: version_migration_item.druid)
+        raise "File not found: #{stacks_path}" unless purl.object_type == 'collection'
+
+        version_migration_item.update(status: 'found_legacy_collection')
+      end
     rescue StandardError => e
       Honeybadger.notify(e, context: { druid: version_migration_item.druid }, tags: 'analysis')
       version_migration_item.update(status: 'error')
--- spec/models/version_migration_item_spec.rb
+++ spec/models/version_migration_item_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe VersionMigrationItem do
     let(:legacy_druid) { 'druid:bc123df4567' }
     let(:versioned_druid) { 'druid:dk120qp2074' }
     let(:not_found_druid) { 'druid:zz699qf3055' }
+    let(:collection_druid) { 'druid:pw528bd2386' }
 
     before do
       FileUtils.mkdir_p DruidTools::PurlDruid.new(legacy_druid, Settings.filesystems.stacks_root).pathname.to_s
@@ -27,6 +28,11 @@ RSpec.describe VersionMigrationItem do
       described_class.create(druid: legacy_druid, status: 'not_analyzed')
       described_class.create(druid: versioned_druid, status: 'not_analyzed')
       described_class.create(druid: not_found_druid, status: 'not_analyzed')
+      described_class.create(druid: collection_druid, status: 'not_analyzed')
+      Purl.create(druid: legacy_druid, object_type: 'item')
+      Purl.create(druid: versioned_druid, object_type: 'item')
+      Purl.create(druid: not_found_druid, object_type: 'item')
+      Purl.create(druid: collection_druid, object_type: 'collection')
       described_class.analyze_purls
     end
 
@@ -34,6 +40,7 @@ RSpec.describe VersionMigrationItem do
       expect(described_class.find_by(druid: legacy_druid).status).to eq 'found_legacy'
       expect(described_class.find_by(druid: versioned_druid).status).to eq 'found_version'
       expect(described_class.find_by(druid: not_found_druid).status).to eq 'error'
+      expect(described_class.find_by(druid: collection_druid).status).to eq 'found_legacy_collection'
     end
   end
 end

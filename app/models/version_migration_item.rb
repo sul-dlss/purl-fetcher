37d53f6486e0f7d8aacd7f73258623cb61f5c0c9
Justin Coyne <jcoyne@justincoyne.com>
2025-08-07 08:47:51 -0500

    Check for legacy metadata only

--- app/models/version_migration_item.rb
+++ app/models/version_migration_item.rb
@@ -1,5 +1,6 @@
 class VersionMigrationItem < ApplicationRecord
-  validates :status, inclusion: { in: ['not_analyzed', 'error', 'found_legacy', 'found_legacy_collection', 'found_version', 'migrated'] }
+  validates :status, inclusion: { in: ['not_analyzed', 'error', 'found_legacy', 'found_legacy_collection',
+                                       'found_legacy_metadata_only', 'found_version', 'migrated'] }
 
   def self.create_all
     Purl.where(deleted_at: nil).find_each do |purl|
@@ -7,8 +8,8 @@ class VersionMigrationItem < ApplicationRecord
     end
   end
 
-  def self.analyze_purls
-    VersionMigrationItem.where(status: 'not_analyzed').find_each do |version_migration_item|
+  def self.analyze_purls(status: 'not_analyzed')
+    VersionMigrationItem.where(status:).find_each do |version_migration_item|
       stacks_path = DruidTools::PurlDruid.new(version_migration_item.druid, Settings.filesystems.stacks_root).pathname
 
       if stacks_path.exist?
@@ -16,14 +17,28 @@ class VersionMigrationItem < ApplicationRecord
         status = Pathname.new(obj.versions_manifest_path).exist? ? 'found_version' : 'found_legacy'
         version_migration_item.update(status:)
       else
-        purl = Purl.find_by(druid: version_migration_item.druid)
-        raise "File not found: #{stacks_path}" unless purl.object_type == 'collection'
-
-        version_migration_item.update(status: 'found_legacy_collection')
+        analyze_one_with_no_stacks(version_migration_item)
       end
     rescue StandardError => e
       Honeybadger.notify(e, context: { druid: version_migration_item.druid }, tags: 'analysis')
       version_migration_item.update(status: 'error')
+    end
+  end
+
+  # Figure out what's going on when there is no stacks path. Is it a collecton or metadata only?
+  def self.analyze_one_with_no_stacks(version_migration_item)
+    purl = Purl.find_by(druid: version_migration_item.druid)
+    if purl.object_type == 'collection'
+      version_migration_item.update(status: 'found_legacy_collection')
+    elsif (cocina_path = "#{purl.purl_druid_path}/cocina.json") && File.exist?(cocina_path)
+      data_hash = JSON.parse(File.read(cocina_path))
+      if data_hash.dig('structural', 'contains').empty?
+        version_migration_item.update(status: 'found_legacy_metadata_only')
+      else
+        version_migration_item.update(status: 'error')
+      end
+    else
+      raise "File not found: #{purl.purl_druid_path}"
     end
   end
 end
--- spec/models/version_migration_item_spec.rb
+++ spec/models/version_migration_item_spec.rb
@@ -17,22 +17,27 @@ RSpec.describe VersionMigrationItem do
     let(:versioned_druid) { 'druid:dk120qp2074' }
     let(:not_found_druid) { 'druid:zz699qf3055' }
     let(:collection_druid) { 'druid:pw528bd2386' }
+    let(:metadata_only_druid) { 'druid:ws105jw7305' }
 
     before do
       FileUtils.mkdir_p DruidTools::PurlDruid.new(legacy_druid, Settings.filesystems.stacks_root).pathname.to_s
 
-      path = VersionedFilesService::Paths.new(druid: versioned_druid)
-      FileUtils.mkdir_p path.versions_path
-      FileUtils.touch path.versions_manifest_path
+      version_paths = VersionedFilesService::Paths.new(druid: versioned_druid)
+      FileUtils.mkdir_p version_paths.versions_path
+      FileUtils.touch version_paths.versions_manifest_path
 
-      described_class.create(druid: legacy_druid, status: 'not_analyzed')
-      described_class.create(druid: versioned_druid, status: 'not_analyzed')
-      described_class.create(druid: not_found_druid, status: 'not_analyzed')
-      described_class.create(druid: collection_druid, status: 'not_analyzed')
-      Purl.create(druid: legacy_druid, object_type: 'item')
-      Purl.create(druid: versioned_druid, object_type: 'item')
-      Purl.create(druid: not_found_druid, object_type: 'item')
+      purl_path = DruidTools::PurlDruid.new(metadata_only_druid, Settings.filesystems.purl_root).pathname
+      FileUtils.mkdir_p purl_path.to_s
+      File.write purl_path / 'cocina.json', '{"structural": {"contains": []}}'
+
+      [legacy_druid, versioned_druid, not_found_druid, collection_druid, metadata_only_druid].each do |druid|
+        described_class.create(druid: druid, status: 'not_analyzed')
+      end
+      [legacy_druid, versioned_druid, not_found_druid, metadata_only_druid].each do |druid|
+        Purl.create(druid:, object_type: 'item')
+      end
       Purl.create(druid: collection_druid, object_type: 'collection')
+
       described_class.analyze_purls
     end
 
@@ -41,6 +46,7 @@ RSpec.describe VersionMigrationItem do
       expect(described_class.find_by(druid: versioned_druid).status).to eq 'found_version'
       expect(described_class.find_by(druid: not_found_druid).status).to eq 'error'
       expect(described_class.find_by(druid: collection_druid).status).to eq 'found_legacy_collection'
+      expect(described_class.find_by(druid: metadata_only_druid).status).to eq 'found_legacy_metadata_only'
     end
   end
 end

7c09d14cdbdb8df80d2219b873fcadbf2e95c0fd
Justin Coyne <jcoyne@justincoyne.com>
2025-09-29 15:19:51 -0500

    Remove unused method

--- app/services/public_xml_writer.rb
+++ app/services/public_xml_writer.rb
@@ -1,8 +1,4 @@
 class PublicXmlWriter
-  def self.write(public_cocina, output_path)
-    File.write(output_path, generate(public_cocina))
-  end
-
   def self.generate(public_cocina)
     thumbnail_service = ThumbnailService.new(public_cocina)
     Publish::PublicXmlService.new(public_cocina:, thumbnail_service:).to_xml
